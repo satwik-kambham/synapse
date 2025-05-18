e1d5f72eeaa77041d9ba316a3e3d525b64a6ba60
Satwik Kambham <satwik.kambham@gmail.com>
2025-05-18 18:23:15 +0530

    combining kokoro audio fragments

--- kokoro_tts.py
+++ kokoro_tts.py
@@ -1,6 +1,6 @@
 from kokoro import KPipeline
 import soundfile as sf
-
+import torch
 
 def load_kokoro():
     pipeline = KPipeline(lang_code="a")
@@ -8,7 +8,9 @@ def load_kokoro():
 
 
 def kokoro_tts(pipeline, text):
-    generator = pipeline(text, voice="af_heart", split_pattern=None)
+    generator = pipeline(text, voice="af_heart")
+    audio_combined = []
     for i, (gs, ps, audio) in enumerate(generator):
-        print(i)
-        sf.write("tts.wav", audio, 24000)
+        audio_combined.append(audio)
+    audio_combined = torch.cat(audio_combined)
+    sf.write("tts.wav", audio_combined, 24000)
